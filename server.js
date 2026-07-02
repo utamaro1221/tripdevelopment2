c1c019661c080b410e1168e2d3089869e8fa9d8c
utamaro1221 <ukimaro395@icloud.com>
2026-07-02 09:33:34 +0900

    api

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const rateLimit = require('express-rate-limit');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('trust proxy', 1);
 app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
